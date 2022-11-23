2d3aa3a469d12eabd8fbe79fb0ff7e417563a091
NowrinIslamMim <nowrin02@gmail.com>
2022-11-24 02:23:25 +0600

    testimonial added

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -4,6 +4,8 @@ import Banner from './Banner';
 import Categories from './Categories';
 import Discount from './Discount';
 import Sale from './Sale';
+import Steps from './Steps';
+import Testimonial from './Testimonial';
 
 const Home = () => {
     return (
@@ -13,6 +15,8 @@ const Home = () => {
             <AdvertisedItems></AdvertisedItems>
             <Sale></Sale>
             <Discount></Discount>
+            <Steps></Steps>
+            <Testimonial></Testimonial>
         </div>
     );
 };
