f2371d1cf2d7702436edccfdae4ce67c50cad46a
NowrinIslamMim <nowrin02@gmail.com>
2022-11-25 17:33:03 +0600

    get all the sellers done

--- src/Components/Pages/Dashboard/AllSellers/AllSellers.js
+++ src/Components/Pages/Dashboard/AllSellers/AllSellers.js
@@ -1,9 +1,62 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import Seller from './Seller';
 
 const AllSellers = () => {
+
+    const { data: sellers, refetch } = useQuery({
+        queryKey: ['sellers'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/sellers');
+            const data = res.json();
+            return data;
+        }
+    })
+
+    const handleDelete = (id) => {
+        fetch(`http://localhost:5000/buyers/${id}`, {
+            method: 'DELETE',
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.deletedCount > 0) {
+                    refetch();
+                }
+            })
+    };
+
+    const handleVerify = (id) => {
+
+    };
     return (
         <div>
-            <h2>All Sellers</h2>
+            <h2 className='text-3xl font-serif'>All Sellers</h2>
+            <div className="overflow-x-auto  w-full">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th>
+                                Profile Pic
+                            </th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            sellers?.map(seller => <Seller
+                                key={seller._id}
+                                seller={seller}
+                                handleDelete={handleDelete}
+                                handleVerify={handleVerify}
+                            ></Seller>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
---
+++ src/Components/Pages/Dashboard/AllSellers/Seller.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Seller = ({ seller, handleDelete, handleVerify }) => {
+
+    const { userName, photoURL, email, _id } = seller;
+    return (
+        <tr>
+            <th>
+                <div className="avatar">
+                    <div className="w-20 rounded">
+                        <img src={photoURL} alt="" />
+                    </div>
+                </div>
+            </th>
+            <td>
+                <div className="flex items-center space-x-3">
+
+                    <div>
+                        <div className="font-bold">{userName}</div>
+                    </div>
+                </div>
+            </td>
+            <td>
+                <span className="badge badge-ghost badge-sm">{email}</span>
+            </td>
+            <th>
+                <button onClick={() => handleDelete(_id)} className="btn btn-primary btn-md">Delete</button>
+            </th>
+            <th>
+                <button onClick={() => handleVerify(_id)} className="btn btn-primary btn-md">Verify</button>
+            </th>
+        </tr>
+    );
+};
+
+export default Seller;
